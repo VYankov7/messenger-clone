34eafa982f780e85d05ca506811b90fd53722c3c
VYankov7 <vyankov02@gmail.com>
2023-06-27 18:30:06 +0300

    update

--- app/components/sidebar/DesktopSidebar.tsx
+++ app/components/sidebar/DesktopSidebar.tsx
@@ -1,12 +1,25 @@
 'use client';
 
-import useRoutes from "@/app/hooks/useRoutes";
 import { useState } from "react";
+import { User } from "@prisma/client";
+
+import useRoutes from "@/app/hooks/useRoutes";
+
+import Avatar from "../Avatar";
 import DesktopItem from "./DesktopItem";
 
-const DesktopSidebar = () => {
+interface DesktopSidebarProps{
+  currentUser: User
+}
+
+const DesktopSidebar: React.FC<DesktopSidebarProps> = ({
+  currentUser
+}) => {
   const routes = useRoutes();
   const [isOpen, setIsOpen] = useState(false);
+
+  console.log({ currentUser })
+
   return (
     <div
       className="
@@ -53,6 +66,24 @@ const DesktopSidebar = () => {
               />
             ))}
         </ul>
+      </nav>
+      <nav className="
+        mt-4
+        flex
+        flex-col
+        justify-between
+        items-center
+      ">
+        <div
+         onClick={() => setIsOpen(true)}
+         className="
+         cursor-pointer
+         hover:opacity-75
+         transition
+         "
+        >
+          <Avatar user={currentUser}/>
+        </div>
       </nav>
     </div>
   );
